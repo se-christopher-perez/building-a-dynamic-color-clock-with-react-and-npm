edf1d44bf9950be6c190e280933ed6654bb0e704
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-05-05 03:01:52 -0400

    added clock and date and tinkered with css

--- color-clock/src/App.jsx
+++ color-clock/src/App.jsx
@@ -1,12 +1,33 @@
 
 import React, { useState, useEffect } from "react"
 import { format } from "date-fns"
+import "./App.css"
 
 function App() {
+
+  const [realTime, setRealTime] = useState(new Date())
+
+  useEffect(() => {
+
+    const clock = setInterval(() => {
+
+      setRealTime(new Date())
+
+    }, 1000)
+
+    return () => clearInterval(clock)
+
+  }, [])
 
   return (
 
     <>
+      <div id="clock-container">
+
+        <p className="clock-day-time">{format(realTime, "eeee, MMMM do, yyyy")}</p>
+        <p className="clock-day-time">{format(realTime, "HH:mm:ss")}</p>
+
+      </div>
 
     </>
 
